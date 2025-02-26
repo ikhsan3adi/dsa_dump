b4fa5eab213bbb4bdb848d9b0c880cde871dc207
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-02-26 10:07:05 +0700

    Praktikum Pointer 4/4 [UNCOMPLETED]

--- Praktikum/Pointer_SDA_P/ptr13.c
+++ Praktikum/Pointer_SDA_P/ptr13.c
@@ -1,7 +1,7 @@
 /* ------------------------------------------------------------------------------------------------------------------ */
-/* File : ptr13.c */
-/* Deskripsi : Pointer ke function (Procedure dengan parameter input/output sebagai parameter) */
-/* Melakukan offset terhadap tabel tergantung fungsi f */
+/* File        : PTR13.cpp          */
+/* Deskripsi   : Pointer ke function (Procedure dengan parameter input/output sebagai parameter) */
+/*       Melakukan offset terhadap tabel tergantung fungsi f     */
 /* ------------------------------------------------------------------------------------------------------------------ */
 
 #include <stdio.h>
@@ -16,14 +16,20 @@ enum MyType
 };
 
 /* Prototype, fungsi yang diberikan sebagai parameter aktual */
-void succI(int *i);  /* suksessor, berupa procedure by ref */
-void predI(int *i);  /* predesessor */
-void succC(char *c); /* suksessor, berupa procedure by ref */
-void predC(char *c); /* predesessor */
-/* print tabel yang belum ketahuan typenya */
-void printtab(void *T, enum MyType Ty);
+void succI(void i);
+/**/ suksessor, berupa procedure by ref * /
+                    void predI(void i);
+/ predesessor * /
+    void succC(void c);
+/ suksessor, berupa procedure by ref * /
+                 void predC(void c);
+/ predesessor * /
+
+    /* print tabel yang belum ketahuan typenya */
+    void printtab(void *T, enum MyType Ty);
+
 /* Prosedur dengan parameter sebuah fungsi */
-void geser(int *TT, void (*f)(void *));
+void geser(void *TT, enum MyType Tty, void (*f)(void *));
 
 /* Program Utama */
 int main()
@@ -37,79 +43,97 @@ int main()
   N = 10;
   MyTabInt = (int *)malloc(N * sizeof(int));
   MyTabC = (char *)malloc(N * sizeof(char));
-
-  *MyTabInt = 1;
+  *(int *)MyTabInt = 1;
+  int *tabInt = (int *)MyTabInt;
+  char *tabC = (char *)MyTabC;
   for (i = 0; i < N; i++)
   {
-    *(MyTabInt + i) = i;
-    *(MyTabC + i) = 'Z';
+    tabInt[i] = i;
+    tabC[i] = 'Z';
   }
-
   printf("Isi tabel dalam main sebelum pemanggilan : \n");
-  printf(" Tabel integer \n");
-  printtab((int *)MyTabInt, 0);
-  printf(" Tabel charakter \n");
-  printtab((char *)MyTabC, 1);
+
+  printf("    Tabel integer \n");
+  printtab(MyTabInt, 0);
+  printf("    Tabel charakter \n");
+  printtab(MyTabC, 1);
+
   printf("\n");
 
   /* Pakai geser dengan parameter succ */
   printf("Geser dengan succ \n");
-  geser((int *)MyTabInt, (int *)succI);
-  geser((char *)MyTabC, (char *)succC);
+  geser(MyTabInt, 0, succI);
+  geser(MyTabC, 1, succC);
   printf(" dalam main \n");
-  printf(" Tabel integer \n");
-  printtab((int *)MyTabInt, 0);
-  printf(" Tabel charakter \n");
-  printtab((char *)MyTabC, 1);
+  printf("    Tabel integer \n");
+  printtab(MyTabInt, 0);
+  printf("    Tabel charakter \n");
+  printtab(MyTabC, 1);
   printf("\n");
 
   /* Pakai geser dengan parameter pred */
   printf("Geser dengan pred \n");
-  geser((int *)MyTabInt, (int *)predI);
-  geser((char *)MyTabC, (char *)predC);
+  geser(MyTabInt, 0, predI);
+  geser(MyTabC, 1, predC);
   printf(" dalam main \n");
-  printf(" Tabel integer \n");
-  printtab((int *)MyTabInt, 0);
-  printf(" Tabel charakter \n");
-  printtab((char *)MyTabC, 1);
+  printf("    Tabel integer \n");
+  printtab(MyTabInt, 0);
+  printf("    Tabel charakter \n");
+  printtab(MyTabC, 1);
   printf("\n");
 
+  free(MyTabInt);
+  free(MyTabC);
   return 0;
 }
 
 /* Body Function */
-void succI(int *i)
+void succI(void *i)
 {
-  *i = *i + 1;
+  int *ip = (int *)i;
+  *ip = *ip + 1;
 }
 
-void predI(int *i)
+void predI(void *i)
 {
-  *i = *i - 1;
+  int *ip = (int *)i;
+  *ip = *ip - 1;
 }
 
-void succC(char *c)
+void succC(void *c)
 {
-  *c = *c + 1;
+  char *cp = (char *)c;
+  *cp = *cp + 1;
 }
 
-void predC(char *c)
+void predC(void *c)
 {
-  *c = *c - 1;
+  char *cp = (char *)c;
+  *cp = *cp - 1;
 }
 
-void geser(int *TT, void (*f)(void *))
+void geser(void *TT, enum MyType Tty, void (*f)(void *))
 {
   int i;
+
   printf(" dalam geser \n");
   for (i = 0; i < N; i++)
   {
-    f(&TT[i]);
-    printf(" %d ", TT[i]);
+    if (Tty == bulat)
+    {
+      int *elem = (int *)TT + i;
+      f(elem);
+      printf(" %d ", *elem);
+    }
+    else if (Tty == karakter)
+    {
+      char *elem = (char *)TT + i;
+      f(elem);
+      printf(" %c ", *elem);
+    }
   }
   printf("\n");
 }
-
 void printtab(void *T, enum MyType Ty)
 {
   int i;
@@ -118,11 +142,12 @@ void printtab(void *T, enum MyType Ty)
     switch (Ty)
     {
     case bulat:
-      printf("%d ", (int *)*(T + i));
+      printf("%d ", ((int *)T)[i]);
       break;
     case karakter:
-      printf("%c ", (char *)*(T + i));
+      printf("%c ", ((char *)T)[i]);
       break;
     }
   }
+  printf("\n");
 }
